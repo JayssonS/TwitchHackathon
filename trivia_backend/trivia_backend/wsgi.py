691e6dc7897a38c9b51d034b6582512d11fc6a91
Solano, Jaysson <jaysson@gatech.edu>
2024-09-22 22:42:24 -0400

    Fix settings module path

--- trivia_backend/trivia_backend/wsgi.py
+++ trivia_backend/trivia_backend/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trivia_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trivia_backend.trivia_backend.settings')
 
 application = get_wsgi_application()
