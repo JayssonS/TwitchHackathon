3884ef2f104835445b7ccca07bd72dff533daf56
Solano, Jaysson <jaysson@gatech.edu>
2024-09-26 02:09:05 -0400

    Switch to logging module for better log capture on Heroku

--- trivia_backend/trivia_backend/battle_bot.py
+++ trivia_backend/trivia_backend/battle_bot.py
@@ -1,7 +1,12 @@
 import os
 import asyncio
+import logging  # Import the logging module
 from twitchio.ext import commands
 from asgiref.sync import sync_to_async
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class BattleBot(commands.Bot):
     def __init__(self, loop):
@@ -9,35 +14,33 @@ class BattleBot(commands.Bot):
                          client_id=os.getenv('TWITCH_BOT_CLIENT_ID'),
                          prefix='!',
                          initial_channels=[])
-        self.loop = loop  # Store the loop for later use
+        self.loop = loop
 
     async def event_ready(self):
-        print(f'Logged in as | {self.nick}')
+        logger.info(f'Logged in as | {self.nick}')
         
         # Fetch channels from the database asynchronously
         channels = await sync_to_async(self.get_channels)()
         
-        # Print fetched channels for debugging
-        print(f"Fetched channels from database: {channels}")
+        # Log fetched channels for debugging
+        logger.info(f"Fetched channels from database: {channels}")
         
         # Join the fetched channels
         for channel in channels:
-            if channel:  # Check if channel is not an empty string
-                print(f"Joining channel: {channel}")
+            if channel:
+                logger.info(f"Joining channel: {channel}")
                 await self.join_channels([channel])
 
     async def event_message(self, message):
         if message.echo:
             return
-        print(f'Received message in {message.channel.name}: {message.content}')
+        logger.info(f'Received message in {message.channel.name}: {message.content}')
 
     def start_bot(self):
         self.loop.run_until_complete(self.start())
 
     def get_channels(self):
-        # Import the model within the method to ensure apps are loaded
         from users.models import TwitchChannel
-        # Fetch all Twitch channels from your database
         channels = list(TwitchChannel.objects.values_list('channel_name', flat=True))
-        print(f"Channels retrieved in get_channels: {channels}")  # Debugging statement
+        logger.info(f"Channels retrieved in get_channels: {channels}")  # Log the retrieved channels
         return channels
